f2ebd8e159c95a670019c34f58a5a23826571945
Jingyi Cui <cuijingyi9685@gmail.com>
2024-03-08 11:56:12 +1030

    add git hub pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// eslint-disable-next-line import/order
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
@@ -20,7 +19,4 @@ root.render(
     </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
